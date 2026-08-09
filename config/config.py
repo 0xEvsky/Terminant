c0d0bb41a26910bc1e33466910276da431611f06
Akram <tubeakram7@gmail.com>
2026-08-09 05:14:25 +0400

    Remove unused parser module and clean up COMMON_FREE_MODELS list

--- config/config.py
+++ config/config.py
@@ -1,5 +1,5 @@
 import os
 
 API_KEY = os.getenv("OPENROUTER_API_KEY")
-COMMON_FREE_MODELS = ["nvidia/nemotron-3-ultra-550b-a55b:free", "poolside/laguna-s-2.1:free"]
+COMMON_FREE_MODELS = ["nvidia/nemotron-3-ultra-550b-a55b:free"]
 DEFAULT_MODEL = COMMON_FREE_MODELS[0]
